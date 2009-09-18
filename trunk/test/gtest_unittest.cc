c22e2cd4a3ea8b7da407f69138269046c8117b48
zhanyong.wan <zhanyong.wan@861a406c-534a-0410-8894-cb66d6ee9925>
2009-09-18 16:35:15 +0000

    Makes gtest compile cleanly with MSVC's warning 4511 & 4512 (copy ctor / assignment operator cannot be generated) enabled.

--- trunk/test/gtest_unittest.cc
+++ trunk/test/gtest_unittest.cc
@@ -6071,6 +6071,8 @@ class SequenceTestingListener : public EmptyTestEventListener {
 
   Vector<String>* vector_;
   const char* const id_;
+
+  GTEST_DISALLOW_COPY_AND_ASSIGN_(SequenceTestingListener);
 };
 
 TEST(EventListenerTest, AppendKeepsOrder) {
